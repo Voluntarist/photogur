d0bff6307812e112fc7544f4881a3aca0ab8e712
Luke Walker <luke.t.walker@gmail.com>
2014-01-29 14:10:06 -0500

    moving pictures into database

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -1,49 +1,13 @@
 class PicturesController < ApplicationController
 
-  def picture0
-    @picture = {
-      :title  => "The old church on the coast of White sea",
-      :artist => "Sergey Ershov",
-      :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-    }
-  end
 
-  def picture1
-    @picture = {
-      :title  => "Sea Power",
-      :artist => "Stephen Scullion",
-      :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-    }
-  end
-
-  def picture2
-    @picture = {
-      :title  => "Into the Poppies",
-      :artist => "John Wilhelm",
-      :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-    }
+  def index
+    @pictures = Picture.all
   end
 
 
   def show
-    @pictures = [
-      {
-        :title => "The old church on the coast of white sea",
-        :artist => "bill",
-        :url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-      },
-      {
-        :title => "sea power",
-        :artist => "john",
-        :url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      },
-      {
-        :title => "Into the Pops",
-        :artist => "sam",
-        :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-      }
-    ]
-    @picture = @pictures[params[:id].to_i]
+    @picture = Picture.find(params[:id])
   end
 
 
