5097341b6eb1fae0f3a86dd8c98c44402821542b
Luke Walker <luke.t.walker@gmail.com>
2014-01-29 14:17:55 -0500

    refactor to add new pix to DB

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -12,10 +12,22 @@ class PicturesController < ApplicationController
 
 
   def new
+    @picture = Picture.new
   end
 
   def create
-    render :text => "Saving a pic. URL: #{params[:url]}, Title: #{params[:title]}, :Artist #{params[:artist]}"
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to pictures_url
+    else
+      render :new
+    end
   end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
+
 
 end
